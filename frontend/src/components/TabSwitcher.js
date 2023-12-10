a588b3693cdb8c7d434a0fcbcfd0660557a9e577
Paras Stefanopoulos <parasstefans@hotmail.com>
2023-12-10 21:48:48 +1100

    updated styling

--- frontend/src/components/TabSwitcher.js
+++ frontend/src/components/TabSwitcher.js
@@ -5,7 +5,7 @@ const TabSwitcher = ({ tabHeaders, activeTab, setActiveTab }) => {
     "inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 hover:cursor-pointer";
 
   return (
-    <ul className="hidden text-sm font-medium text-center text-gray-500 shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+    <ul className="text-sm font-medium text-center text-gray-500 shadow flex dark:divide-gray-700 dark:text-gray-400">
       {tabHeaders.map((tabName) => (
         <li key={tabName} className="w-full">
           <div
