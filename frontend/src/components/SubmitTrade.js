36df32b31fcdd1a124cc45e256f1947ea6502ef7
Paras Stefanopoulos <parasstefans@hotmail.com>
2024-01-24 23:05:47 +1100

    ui updates

--- frontend/src/components/SubmitTrade.js
+++ frontend/src/components/SubmitTrade.js
@@ -1,76 +1,87 @@
 import React, { useState } from "react";
 import UserSearch from "./UserSearch";
 
-const AdjustableContent = ({ adjustFunc, setFunc, value }) => {
+const AdjustableContent = ({ adjustFunc, setFunc, value, enableButtons }) => {
   return (
     <>
-      <div className="flex justify-center space-x-2 mb-4">
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
-          onClick={() => adjustFunc(-100)}
-        >
-          -100
-        </button>
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
-          onClick={() => adjustFunc(-10)}
-        >
-          -10
-        </button>
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
-          onClick={() => adjustFunc(-1)}
-        >
-          -1
-        </button>
-      </div>
+      {enableButtons && (
+        <div className="flex justify-center space-x-2 mb-4">
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
+            onClick={() => adjustFunc(-100)}
+          >
+            -100
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
+            onClick={() => adjustFunc(-10)}
+          >
+            -10
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
+            onClick={() => adjustFunc(-1)}
+          >
+            -1
+          </button>
+        </div>
+      )}
       <div className="flex items-center mb-4">
         <input
           className="flex-1 border-gray-500 text-center"
+          pattern="[0-9]*"
+          inputMode="numeric"
           type="number"
           value={value}
-          onChange={(e) => setFunc(Math.max(0, e.target.value))}
+          onChange={(e) => {
+            if (!e.target.value) setFunc(0);
+            else {
+              setFunc(String(Math.max(0, Number.parseInt(e.target.value))));
+            }
+          }}
         />
       </div>
-      <div className="flex justify-center space-x-2 mb-4">
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
-          onClick={() => adjustFunc(100)}
-        >
-          +100
-        </button>
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
-          onClick={() => adjustFunc(10)}
-        >
-          +10
-        </button>
+      {enableButtons && (
+        <div className="flex justify-center space-x-2 mb-4">
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
+            onClick={() => adjustFunc(100)}
+          >
+            +100
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
+            onClick={() => adjustFunc(10)}
+          >
+            +10
+          </button>
 
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
-          onClick={() => adjustFunc(1)}
-        >
-          +1
-        </button>
-      </div>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-20"
+            onClick={() => adjustFunc(1)}
+          >
+            +1
+          </button>
+        </div>
+      )}
     </>
   );
 };
 
 const SubmitTrade = ({ onSubmitTrade, sessionState }) => {
   const [counterPartyName, setCounterPartyName] = useState("");
-  const [volume, setVolume] = useState(1);
+  const [volume, setVolume] = useState(0);
   const [price, setPrice] = useState(0);
   const [tradeType, setTradeType] = useState("");
-
-  console.log(sessionState);
+  const [isQuickAccessButtonsEnabled, setIsQuickAccessButtonsEnabled] =
+    useState(true);
 
   const username = sessionState.username;
   const session_id = sessionState.session_id;
   const users = Object.keys(sessionState.users);
 
   const adjustVolume = (amount) => {
-    setVolume((prevVolume) => Math.max(1, prevVolume + amount));
+    setVolume((prevVolume) => Math.max(0, prevVolume + amount));
   };
 
   const adjustPrice = (amount) => {
@@ -89,7 +100,7 @@ const SubmitTrade = ({ onSubmitTrade, sessionState }) => {
       onSubmitTrade(trade_data);
       // Reset fields after submission
       setCounterPartyName("");
-      setVolume(1);
+      setVolume(0);
       setPrice(0);
       setTradeType("");
     } else {
@@ -99,34 +110,11 @@ const SubmitTrade = ({ onSubmitTrade, sessionState }) => {
 
   return (
     <div className="mb-4 grid grid-cols-1 divide-y">
-      {/* <input
-        className="w-full mb-2 px-3 py-2 border rounded"
-        type="text"
-        placeholder="Counter Party Name"
-        value={counterPartyName}
-        onChange={(e) => setCounterPartyName(e.target.value)}
-      /> */}
       <UserSearch
         setCounterPartyName={setCounterPartyName}
         counterPartyName={counterPartyName}
         users={users.filter((user) => user !== username)}
       />
-      <div>
-        <div className="font-bold mb-2 text-center">Volume</div>
-        <AdjustableContent
-          adjustFunc={adjustVolume}
-          setFunc={setVolume}
-          value={volume}
-        />
-      </div>
-      <div>
-        <div className="font-bold mb-2 text-center">Price</div>
-        <AdjustableContent
-          adjustFunc={adjustPrice}
-          setFunc={setPrice}
-          value={price}
-        />
-      </div>
       <div>
         <div className="font-bold mb-2 text-center">Direction</div>
         <div className="flex justify-center space-x-2 mb-4">
@@ -151,13 +139,45 @@ const SubmitTrade = ({ onSubmitTrade, sessionState }) => {
             Sell
           </button>
         </div>
+      </div>
+      <div>
+        <div className="font-bold mb-2 text-center">Volume</div>
+        <AdjustableContent
+          enableButtons={isQuickAccessButtonsEnabled}
+          adjustFunc={adjustVolume}
+          setFunc={setVolume}
+          value={volume}
+        />
+      </div>
+      <div>
+        <div className="font-bold mb-2 text-center">Price</div>
+        <AdjustableContent
+          enableButtons={isQuickAccessButtonsEnabled}
+          adjustFunc={adjustPrice}
+          setFunc={setPrice}
+          value={price}
+        />
+      </div>
+      <div>
         <button
           className="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-500"
           onClick={submitTrade}
-          disabled={sessionState.inplay === false}
+          disabled={sessionState.inplay === false || volume <= 0}
         >
           {sessionState.inplay ? "Submit Trade" : "Game not in play."}
         </button>
+      </div>
+      <div className="flex items-center mb-4 pt-6">
+        <input
+          id="admin-checkbox"
+          type="checkbox"
+          className="mr-2"
+          checked={isQuickAccessButtonsEnabled}
+          onChange={(e) => setIsQuickAccessButtonsEnabled(e.target.checked)}
+        />
+        <label htmlFor="enableButtons-checkbox">
+          Enable quick-access buttons
+        </label>
       </div>
     </div>
   );
